d3409cd729a7a57a289c8c990f2936d0e78d147a
toonlink <toonlink@riseup.net>
2023-04-17 18:56:05 -0400

    initial

---
+++ discord.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+type DiscordInstance struct {
+	// Path to "resources" directory
+	Path    string
+	Channel string
+}
+
+func GetInstance(channel string) (DiscordInstance, error) {
+	channelString := "Discord"
+
+	// Generate channel strings (e.g discord-canary, DiscordCanary, Discord Canary)
+	if channel != "Stable" {
+		switch os := runtime.GOOS; os {
+		case "darwin":
+			channelString = channelString + " " + channel
+		case "windows":
+			channelString = channelString + channel
+		default: // Linux and BSD are basically the same thing
+			channelString = channelString + "-" + channel
+		}
+	}
+
+	instance := DiscordInstance{
+		Path:    "",
+		Channel: channel,
+	}
+
+	switch OS := runtime.GOOS; OS {
+	case "darwin":
+		instance.Path = filepath.Join("/Applications", channelString+".app", "Contents", "Resources")
+	case "windows":
+		starterPath := filepath.Join(os.Getenv("localappdata"), channelString, "/")
+		filepath.Walk(starterPath, func(path string, _ fs.FileInfo, _ error) error {
+
+			if strings.HasPrefix(filepath.Base(path), "app-") {
+				instance.Path = filepath.Join(path, "resources")
+			}
+
+			return nil
+		})
+	default: // Linux and BSD are *still* basically the same thing
+		channels := []string{channelString, strings.ToLower(channelString)}
+		path := os.Getenv("PATH")
+
+		for _, channel := range channels {
+			for _, pathItem := range strings.Split(path, ":") {
+				joinedPath := filepath.Join(pathItem, channel)
+				if _, err := os.Stat(joinedPath); err == nil {
+					possiblepath, _ := filepath.EvalSymlinks(joinedPath)
+					if possiblepath != joinedPath {
+						instance.Path = filepath.Join(possiblepath, "..", "resources")
+					}
+				}
+			}
+		}
+
+		if instance.Path == "" && channel == "Stable" {
+			instance.Path = "/var/lib/flatpak/app/com.discordapp.Discord/x86_64/stable/active/files/discord/resources/"
+		}
+	}
+
+	if _, err := os.Stat(instance.Path); err == nil {
+		return instance, nil
+	} else {
+		return instance, errors.New("Instance doesn't exist")
+	}
+}
+
+func GetChannels() []DiscordInstance {
+	possible := []string{"Stable", "PTB", "Canary", "Development"}
+	var channels []DiscordInstance
+
+	for _, channel := range possible {
+		c, err := GetInstance(channel)
+		if err == nil {
+			channels = append(channels, c)
+		}
+	}
+
+	return channels
+}
+
+func NewDiscordInstance(path string) (*DiscordInstance, error) {
+	instance := DiscordInstance{
+		Path:    path,
+		Channel: "Unknown",
+	}
+
+	if _, err := os.Stat(filepath.Join(instance.Path, "app.asar")); err == nil {
+		return &instance, nil
+	} else {
+		return nil, errors.New("Instance doesn't exist")
+	}
+}
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"image/color"
+	"io"
+	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/sqweek/dialog"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if user.Username != "root" && runtime.GOOS == "linux" {
+		dialog.Message("%s", "Installing shelter requires root permissions. Please rerun shelter installer as root to continue.").Error()
+		return
+	}
+
+	a := app.New()
+	a.Settings().SetTheme(discordTheme{})
+	w := a.NewWindow("shelter installer")
+	w.SetFixedSize(true)
+
+	selectedInstancePath := make([]string, 1)
+
+	instances := make(map[string]string)
+	var channels []string
+	for _, instance := range GetChannels() {
+		instances[instance.Channel] = instance.Path
+
+		channels = append(channels, instance.Channel)
+	}
+
+	header := canvas.NewText("shelter installer", color.White)
+	header.TextSize = 22
+	header.Alignment = fyne.TextAlignCenter
+
+	description := widget.NewLabel("Choose the version of Discord you'd like to install to, then click install.")
+
+	installButton := widget.NewButton("Install", func() {})
+	installButton.Disable()
+
+	installedContainer := container.NewVBox()
+
+	updateButton := widget.NewButton("Update", func() {})
+	uninstallButton := widget.NewButton("Uninstall", func() {})
+
+	installedContainer.Add(updateButton)
+	installedContainer.Add(uninstallButton)
+
+	installedContainer.Hide()
+
+	showInstall := func() {
+		installButton.Show()
+		installButton.Enable()
+		installedContainer.Hide()
+
+		w.Resize(fyne.NewSize(0, 0))
+	}
+
+	showUninstall := func() {
+		installButton.Hide()
+		installButton.Disable()
+		installedContainer.Show()
+
+		w.Resize(fyne.NewSize(0, 0))
+	}
+
+	installButton.OnTapped = func() {
+		shelterZip, err := os.CreateTemp("", "shelter.zip")
+		if err != nil {
+			a.Quit()
+		}
+
+		tempDirectory := os.TempDir()
+
+		resp, err := http.Get("https://github.com/uwu/shelter/archive/refs/heads/main.zip")
+		if err != nil {
+			a.Quit()
+		}
+		defer resp.Body.Close()
+
+		_, err = io.Copy(shelterZip, resp.Body)
+
+		Unzip(shelterZip.Name(), tempDirectory)
+		os.Remove(shelterZip.Name())
+
+		shelterDir := filepath.Join(tempDirectory, "shelter-main")
+		injectorPath := filepath.Join(shelterDir, "injectors/desktop/app")
+
+		os.Rename(injectorPath, filepath.Join(selectedInstancePath[0], "app"))
+		os.Remove(shelterDir)
+
+		os.Rename(filepath.Join(selectedInstancePath[0], "app.asar"), filepath.Join(selectedInstancePath[0], "original.asar"))
+
+		showUninstall()
+	}
+
+	updateButton.OnTapped = func() {
+		os.RemoveAll(filepath.Join(selectedInstancePath[0], "app"))
+		installButton.OnTapped()
+	}
+
+	uninstallButton.OnTapped = func() {
+		os.RemoveAll(filepath.Join(selectedInstancePath[0], "app"))
+		os.Rename(filepath.Join(selectedInstancePath[0], "original.asar"), filepath.Join(selectedInstancePath[0], "app.asar"))
+
+		showInstall()
+	}
+
+	w.SetContent(container.NewVBox(
+		header,
+		description,
+		widget.NewSelect(channels, func(s string) {
+			selectedInstancePath[0] = instances[s]
+
+			if _, err := os.Stat(filepath.Join(selectedInstancePath[0], "original.asar")); os.IsNotExist(err) {
+				showInstall()
+			} else {
+				showUninstall()
+			}
+		}),
+		installButton,
+		installedContainer,
+	))
+
+	w.ShowAndRun()
+}
